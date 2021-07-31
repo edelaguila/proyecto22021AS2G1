f65c9b1606de5cc1d9bf47064c44055ade90825b
KevinRolandoGonzalezRamirez <kgonzalezr13@miumg.edu.gt>
2021-07-31 16:05:23 -0600

    Creado metodo de apertura de conexion

---
+++ CapaModelo/conexion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Odbc;
+using MySql.Data.MySqlClient;  //abre la conexion de mysql
+
+namespace CapaModelo
+{
+    class conexion
+    { 
+        //abre la conexion si es posible de lo controrario no lo abrir y no enviara nada
+
+        public MySqlConnection abreConexion()
+        {
+
+            string MyConnection2 = "datasource=localhost;port=3306;username=root;password=123456789;database=cinevision";
+            //This is my insert query in which i am taking input from the user through windows forms  
+            //string Query = "insert into usuarios(Usuario,Password) values('" + this.t1.Text + "','" + this.t2.Text + "');";
+            //This is  MySqlConnection here i have created the object and pass my connection string.  
+            MySqlConnection MyConn2 = new MySqlConnection(MyConnection2);
+
+            try
+            {
+                MyConn2.Open();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("No se logro abrir la conexion");
+            }
+
+            return MyConn2;
+        }
+    }
+}
--- CapaVista/PruebaBD.Designer.cs
+++ CapaVista/PruebaBD.Designer.cs
@@ -29,33 +29,70 @@ namespace CapaVista
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.t1 = new System.Windows.Forms.TextBox();
+            this.t2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.boton_enviar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox1
+            // t1
             // 
-            this.textBox1.Location = new System.Drawing.Point(50, 73);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 0;
+            this.t1.Location = new System.Drawing.Point(115, 49);
+            this.t1.Name = "t1";
+            this.t1.Size = new System.Drawing.Size(100, 20);
+            this.t1.TabIndex = 0;
+            this.t1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // textBox2
+            // t2
             // 
-            this.textBox2.Location = new System.Drawing.Point(50, 109);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 1;
+            this.t2.Location = new System.Drawing.Point(115, 99);
+            this.t2.Name = "t2";
+            this.t2.Size = new System.Drawing.Size(100, 20);
+            this.t2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "usuario:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "pass:";
+            // 
+            // boton_enviar
+            // 
+            this.boton_enviar.ForeColor = System.Drawing.Color.DarkCyan;
+            this.boton_enviar.Location = new System.Drawing.Point(260, 246);
+            this.boton_enviar.Name = "boton_enviar";
+            this.boton_enviar.Size = new System.Drawing.Size(190, 70);
+            this.boton_enviar.TabIndex = 4;
+            this.boton_enviar.Text = "enviar ";
+            this.boton_enviar.UseVisualStyleBackColor = true;
+            this.boton_enviar.Click += new System.EventHandler(this.boton_enviar_Click);
             // 
             // PruebaBD
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(719, 383);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.boton_enviar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.t2);
+            this.Controls.Add(this.t1);
             this.Name = "PruebaBD";
             this.Text = "PruebaBD";
+            this.Load += new System.EventHandler(this.PruebaBD_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -63,7 +100,10 @@ namespace CapaVista
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox t1;
+        private System.Windows.Forms.TextBox t2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button boton_enviar;
     }
 }
