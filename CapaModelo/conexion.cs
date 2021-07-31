f6609e728852a89ae3628756f719c1af25cc9344
KevinRolandoGonzalezRamirez <kgonzalezr13@miumg.edu.gt>
2021-07-31 16:13:38 -0600

    Metodo para cerrar conexión implementado

--- CapaModelo/conexion.cs
+++ CapaModelo/conexion.cs
@@ -16,9 +16,7 @@ namespace CapaModelo
         {
 
             string MyConnection2 = "datasource=localhost;port=3306;username=root;password=123456789;database=cinevision";
-            //This is my insert query in which i am taking input from the user through windows forms  
-            //string Query = "insert into usuarios(Usuario,Password) values('" + this.t1.Text + "','" + this.t2.Text + "');";
-            //This is  MySqlConnection here i have created the object and pass my connection string.  
+             
             MySqlConnection MyConn2 = new MySqlConnection(MyConnection2);
 
             try
@@ -31,6 +29,20 @@ namespace CapaModelo
             }
 
             return MyConn2;
+        }
+
+
+        public void cierraConexion(MySqlConnection cerrar)
+        {
+            try
+            {
+                cerrar.Close();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("No se logro cerrar la conexion");
+            }
+
         }
     }
 }
