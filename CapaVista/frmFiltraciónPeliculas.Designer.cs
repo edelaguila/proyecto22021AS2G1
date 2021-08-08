ac9df86458c4a8c70089ce4ed07fe9fd9f4a1761
gcoyoyz <gcoyoyz@miumg.edu.gt>
2021-08-07 22:38:22 -0600

    butacas 2.0

--- CapaVista/frmFiltraciónPeliculas.Designer.cs
+++ CapaVista/frmFiltraciónPeliculas.Designer.cs
@@ -56,17 +56,19 @@ namespace CapaVista
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(274, 226);
+            this.dateTimePicker1.Location = new System.Drawing.Point(365, 278);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.Size = new System.Drawing.Size(265, 22);
             this.dateTimePicker1.TabIndex = 73;
             // 
             // btnRegreso
             // 
             this.btnRegreso.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRegreso.Location = new System.Drawing.Point(24, 526);
+            this.btnRegreso.Location = new System.Drawing.Point(32, 647);
+            this.btnRegreso.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnRegreso.Name = "btnRegreso";
-            this.btnRegreso.Size = new System.Drawing.Size(75, 23);
+            this.btnRegreso.Size = new System.Drawing.Size(100, 28);
             this.btnRegreso.TabIndex = 72;
             this.btnRegreso.Text = "Regresar";
             this.btnRegreso.UseVisualStyleBackColor = true;
@@ -74,9 +76,10 @@ namespace CapaVista
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(656, 185);
+            this.pictureBox1.Location = new System.Drawing.Point(875, 228);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(217, 277);
+            this.pictureBox1.Size = new System.Drawing.Size(289, 341);
             this.pictureBox1.TabIndex = 71;
             this.pictureBox1.TabStop = false;
             // 
@@ -85,9 +88,10 @@ namespace CapaVista
             this.label7.AutoSize = true;
             this.label7.BackColor = System.Drawing.SystemColors.HotTrack;
             this.label7.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(618, 103);
+            this.label7.Location = new System.Drawing.Point(824, 127);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(302, 40);
+            this.label7.Size = new System.Drawing.Size(386, 50);
             this.label7.TabIndex = 70;
             this.label7.Text = "Peliculas Disponibles";
             // 
@@ -95,9 +99,10 @@ namespace CapaVista
             // 
             this.panel3.BackColor = System.Drawing.Color.MediumBlue;
             this.panel3.Controls.Add(this.label2);
-            this.panel3.Location = new System.Drawing.Point(2, 2);
+            this.panel3.Location = new System.Drawing.Point(3, 2);
+            this.panel3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(1028, 53);
+            this.panel3.Size = new System.Drawing.Size(1371, 65);
             this.panel3.TabIndex = 69;
             // 
             // label2
@@ -106,9 +111,10 @@ namespace CapaVista
             this.label2.BackColor = System.Drawing.Color.MediumBlue;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Location = new System.Drawing.Point(4, 0);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(278, 40);
+            this.label2.Size = new System.Drawing.Size(356, 50);
             this.label2.TabIndex = 2;
             this.label2.Text = "Filtrado de Película";
             // 
@@ -116,9 +122,10 @@ namespace CapaVista
             // 
             this.button2.BackColor = System.Drawing.Color.OrangeRed;
             this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(274, 347);
+            this.button2.Location = new System.Drawing.Point(365, 427);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(103, 45);
+            this.button2.Size = new System.Drawing.Size(137, 55);
             this.button2.TabIndex = 68;
             this.button2.Text = "LIMPIAR";
             this.button2.UseVisualStyleBackColor = false;
@@ -126,9 +133,10 @@ namespace CapaVista
             // comboBox7
             // 
             this.comboBox7.FormattingEnabled = true;
-            this.comboBox7.Location = new System.Drawing.Point(52, 426);
+            this.comboBox7.Location = new System.Drawing.Point(69, 524);
+            this.comboBox7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBox7.Name = "comboBox7";
-            this.comboBox7.Size = new System.Drawing.Size(152, 21);
+            this.comboBox7.Size = new System.Drawing.Size(201, 24);
             this.comboBox7.TabIndex = 66;
             // 
             // label8
@@ -136,18 +144,20 @@ namespace CapaVista
             this.label8.AutoSize = true;
             this.label8.BackColor = System.Drawing.SystemColors.HotTrack;
             this.label8.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(54, 389);
+            this.label8.Location = new System.Drawing.Point(72, 479);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(64, 21);
+            this.label8.Size = new System.Drawing.Size(77, 28);
             this.label8.TabIndex = 65;
             this.label8.Text = "Idioma";
             // 
             // comboBox5
             // 
             this.comboBox5.FormattingEnabled = true;
-            this.comboBox5.Location = new System.Drawing.Point(52, 337);
+            this.comboBox5.Location = new System.Drawing.Point(69, 415);
+            this.comboBox5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBox5.Name = "comboBox5";
-            this.comboBox5.Size = new System.Drawing.Size(152, 21);
+            this.comboBox5.Size = new System.Drawing.Size(201, 24);
             this.comboBox5.TabIndex = 64;
             // 
             // label6
@@ -155,9 +165,10 @@ namespace CapaVista
             this.label6.AutoSize = true;
             this.label6.BackColor = System.Drawing.SystemColors.HotTrack;
             this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(48, 295);
+            this.label6.Location = new System.Drawing.Point(64, 363);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(156, 21);
+            this.label6.Size = new System.Drawing.Size(193, 28);
             this.label6.TabIndex = 63;
             this.label6.Text = "Tipo de Proyección";
             // 
@@ -166,18 +177,20 @@ namespace CapaVista
             this.label5.AutoSize = true;
             this.label5.BackColor = System.Drawing.SystemColors.HotTrack;
             this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(288, 185);
+            this.label5.Location = new System.Drawing.Point(384, 228);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(174, 21);
+            this.label5.Size = new System.Drawing.Size(216, 28);
             this.label5.TabIndex = 62;
             this.label5.Text = "Fecha de Reservación";
             // 
             // comboBox3
             // 
             this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(52, 225);
+            this.comboBox3.Location = new System.Drawing.Point(69, 277);
+            this.comboBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(152, 21);
+            this.comboBox3.Size = new System.Drawing.Size(201, 24);
             this.comboBox3.TabIndex = 61;
             // 
             // label4
@@ -185,18 +198,20 @@ namespace CapaVista
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.SystemColors.HotTrack;
             this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(48, 185);
+            this.label4.Location = new System.Drawing.Point(64, 228);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(162, 21);
+            this.label4.Size = new System.Drawing.Size(199, 28);
             this.label4.TabIndex = 60;
             this.label4.Text = "Formato de Película";
             // 
             // comboBox2
             // 
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(292, 121);
+            this.comboBox2.Location = new System.Drawing.Point(389, 149);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(152, 21);
+            this.comboBox2.Size = new System.Drawing.Size(201, 24);
             this.comboBox2.TabIndex = 59;
             // 
             // label3
@@ -204,9 +219,10 @@ namespace CapaVista
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.SystemColors.HotTrack;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(298, 89);
+            this.label3.Location = new System.Drawing.Point(397, 110);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(68, 21);
+            this.label3.Size = new System.Drawing.Size(84, 28);
             this.label3.TabIndex = 58;
             this.label3.Text = "Horario";
             // 
@@ -215,9 +231,10 @@ namespace CapaVista
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.SystemColors.HotTrack;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(54, 89);
+            this.label1.Location = new System.Drawing.Point(72, 110);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(91, 21);
+            this.label1.Size = new System.Drawing.Size(110, 28);
             this.label1.TabIndex = 57;
             this.label1.Text = "Complejos";
             // 
@@ -225,35 +242,40 @@ namespace CapaVista
             // 
             this.button1.BackColor = System.Drawing.Color.OrangeRed;
             this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(424, 347);
+            this.button1.Location = new System.Drawing.Point(565, 427);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(103, 45);
+            this.button1.Size = new System.Drawing.Size(137, 55);
             this.button1.TabIndex = 55;
             this.button1.Text = "BUSCAR";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // dataGridView1
             // 
             this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.HotTrack;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 76);
+            this.dataGridView1.Location = new System.Drawing.Point(16, 94);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(991, 499);
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(1321, 614);
             this.dataGridView1.TabIndex = 67;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(41, 121);
+            this.textBox1.Location = new System.Drawing.Point(55, 149);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(163, 20);
+            this.textBox1.Size = new System.Drawing.Size(216, 22);
             this.textBox1.TabIndex = 74;
             // 
             // frmFiltraciónPeliculas
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(1011, 586);
+            this.ClientSize = new System.Drawing.Size(1348, 721);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.btnRegreso);
@@ -273,6 +295,7 @@ namespace CapaVista
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmFiltraciónPeliculas";
             this.Text = "frmFiltraciónPeliculas";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
